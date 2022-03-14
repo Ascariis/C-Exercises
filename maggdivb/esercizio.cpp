5d0ad0a4f4b9df252839ba7a1015038197fa1164
Ascariis <albertoporcu4@gmail.com>
2022-03-14 18:03:22 +0100

    carlo

---
+++ maggdivb/esercizio.cpp
@@ -0,0 +1,22 @@
+/* You are given two numbers a and b. Create a function that returns the next number greater than a and b and divisible by b. */
+// 17 8 -> 24 
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    int a,b;
+    cout<<"Inserire il valore per 'a': ";
+    cin>>a;
+    cout<<"Inserire il valore per 'b': ";
+    cin>>b;
+
+    while(a%b!=0)
+    {
+        a++;        
+    }
+
+    cout<<a;
+
+}
