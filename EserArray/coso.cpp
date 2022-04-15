9b55fdd4dba7b229c131fa9e1de8b35d3c06535b
Ascariis <68233308+Ascariis@users.noreply.github.com>
2022-04-15 19:08:12 +0200

    piuPresente funzionate

--- EserArray/coso.cpp
+++ EserArray/coso.cpp
@@ -8,19 +8,21 @@ int ar[numElementi];
 
 void menuScelta()
 {
-    cout<<"1) Visualizzarlo in ordine crescente;"<<endl;
-    cout<<"2) Visualizzarlo in ordine decrescente;"<<endl;
-    cout<<"3) Visualizzare il numero maggiore;"<<endl;
-    cout<<"4) Visualizzare il numero minore;"<<endl;
-    cout<<"5) Visualizzare il numero più presente all’interno dell’array;"<<endl;
-    cout<<"6) Visualizzare la somma di tutti gli elementi;"<<endl;
-    cout<<"7) Visualizzare l’elenco di tutti gli elementi pari;"<<endl;
-    cout<<"8) Visualizzare tutti i valori dell’Array maggiori del numero inserito;"<<endl;
-    cout<<"9) Dire se nell’array sono presenti almeno 7 valori distinti;"<<endl;
-    cout<<"10) Sostituire i valori negativi dell'array con la scritta 'NEG';"<<endl;
-    cout<<"11) Creare un secondo array la cui grandezza è pari alla quantità di numeri positivi presente nell’array originale,"<<endl; 
-    cout<<"12) Numero di elementi positivi, negativi o nulli nell'array;"<<endl;
-    cout<<"13) Sostituire tutti gli elementi dell’array."<<endl;
+    cout<<"\n";
+    cout<<"1. Visualizza in ordine crescente"<<endl;
+    cout<<"2. Visualizza in ordine decrescente"<<endl;
+    cout<<"3. Visualizza il numero maggiore"<<endl;
+    cout<<"4. Visualizza il numero minore"<<endl;
+    cout<<"5. Visualizza il numero più presente all’interno dell’array"<<endl;
+    cout<<"6. Visualizza la somma di tutti gli elementi"<<endl;
+    cout<<"7. Visualizza l’elenco di tutti gli elementi pari"<<endl;
+    cout<<"8. Visualizza tutti i valori dell’Array maggiori del numero inserito"<<endl;
+    cout<<"9. Controlla se presenta 7 valori distinti"<<endl;
+    cout<<"10. Sostituisci i valori negativi dell'array con 'NEG'"<<endl;
+    cout<<"11. Crea un nuovo array in base ai valori positivi del primo array"<<endl; 
+    cout<<"12. Numero di elementi positivi, negativi o nulli nell'array"<<endl;
+    cout<<"13. Sostituire tutti gli elementi dell’array"<<endl;
+    cout<<"\n";
 }
 
 void riempiArray()
@@ -78,7 +80,7 @@ void numMaggiore()
             max = ar[x];
         }
     printArray();
-    cout << "Il numero maggiore è " << max;
+    cout << "Il numero maggiore è " << max<<endl;
 }
 
 void numMinore()
@@ -86,34 +88,39 @@ void numMinore()
     int min = ar[0];
     for (int x = 1; x < numElementi; x++)
         if (ar[x] < min)
-        {
             min = ar[x];
-        }
+    
     printArray();
-    cout << "Il numero maggiore è " << min;
+    cout << "Il numero minore è " << min<<endl;
 }
 
-void piuPresente() // !!!!WIP!!!!
+void piuPresente()
 {
-    int qtArray[numElementi];
-    
-    for (int i = 0; i < numElementi - 1; i++)
-        qtArray[i]=0;
-    
     arrCrescente();
-    printArray();
-    for (int x = 0; x < numElementi - 1; x++)
-        for (int i = x+1; i < numElementi; i++)
-        {
-            if (ar[x] == ar[i])
-                qtArray[x]+=1;
+     
+    int max = 1, ris = ar[0], curr = 1;
+    for (int i = 1; i < numElementi; i++) 
+    {
+        if (ar[i] == ar[i - 1])
+            curr++;
+        else {
+            if (curr > max) 
+            {
+                max = curr;
+                ris = ar[i - 1];
+            }
+            curr = 1;
         }
-    int max;
-    for (int x = 0; x < numElementi; x++)
-        for (int i = x+1; i < numElementi; i++);
-            if (qtArray[y]>qtArray[x])
-                
-    cout << "\n";
+    }
+ 
+    if (curr > max)
+    {
+        max = curr;
+        ris = ar[numElementi - 1];
+    }
+
+    cout<<"Il numero più presente nell'array è: "<<ris<<endl;
+
 }
 
 void arrSomma()
@@ -143,6 +150,9 @@ void maggInserito()
     for (int i = 0; i < numElementi; i++)
         if (ar[i] > valore)
             cout << ar[i] << " ";
+        else
+            break;
+    cout<<"Nessun numero è maggiore di quello inserito.";
     cout << "\n";
 }
 
@@ -190,6 +200,45 @@ void numPosNegNull()
     cout<<"Ci sono "<<pos<<" numeri positivi, "<<neg<<" numeri negativi e "<<null<<" nulli. \n";
 }
 
+void arNumPos()
+{
+    int size = 0;
+    arrCrescente();
+
+    for (int i = 0; i < numElementi; i++)
+        if(ar[i] > 0)
+            size++;
+
+    int arPos[size];
+
+    for (int i = 0; i < numElementi; i++)
+        if (ar[i]>0)
+        {
+            arPos[i]=ar[i];
+            cout<<arPos[i]<<" ";
+        }
+    cout<<endl;
+}
+
+void arrayUtente()
+{
+    int min,max;
+    
+    cout<<"Inserire il valore minimo: \n";
+    cin>>min;
+    cout<<"Inserire il numero massimo: \n";
+    cin>>max;
+
+    int range = max -min + 1;
+    
+    for (int i = 0; i < numElementi; i++)
+    {
+        ar[i]=rand() % range + min;
+        cout<<ar[i]<<" ";
+    }
+    cout<<endl;
+}
+
 int main()
 {   
     char risposta;
@@ -201,14 +250,14 @@ int main()
         
         riempiArray();
         
-        cout<<"### Programma multiuso sugli array ###"<<endl;
+        cout<<"<----- Programma multiuso sugli array ----->"<<endl;
         menuScelta();
         
         do
         {
-            cout<<"Inserire la propria scelta [1-13]: ";
+            cout<<"0. Quit\nInserire la propria scelta [1-13]: ";
             cin>>scelta;
-        } while (scelta < 1 || scelta > 13);
+        } while (scelta < 0 || scelta > 13);
         
         switch (scelta)
         {
@@ -243,21 +292,23 @@ int main()
             printNeg();
             break;
         case 11:
-            arrCrescente();
+            arNumPos();
             break;
         case 12:
             numPosNegNull();
             break;
         case 13:
-            arrCrescente();
+            arrayUtente();
             break;
 
         default:
+            risposta='n';
             break;
+                
         }
 
         cout<<"Vuoi continuare con un'altra operazione? [Y/n] ";
         cin>>risposta;
 
-    } while (risposta=='y');
+    } while (risposta!='n' || risposta=='y');
 }
