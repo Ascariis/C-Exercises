caf446fad98ebe56a5d3d53f17e380c51821ee21
Ascariis <68233308+Ascariis@users.noreply.github.com>
2022-04-24 21:53:15 +0200

    Organizz.

--- Varie/Fibonacci/Fibonacci.cpp
+++ Varie/Fibonacci/Fibonacci.cpp
@@ -9,15 +9,15 @@ int main() {
   for (int num = 1; num < 1000; ++num) {
     // Primi due termini.
     if (num == 1) {
-      cout << numPrec1 << ", ";
+      cout << numPrec1 << " ";
     }
     if (num == 2) {
-      cout << numPrec2 << ", ";
+      cout << numPrec2 << " ";
     }
     num = numPrec1 + numPrec2;
     numPrec1 = numPrec2;
     numPrec2 = num;
 
-    cout << num << ", ";
+    cout << num << " ";
   }
 }
