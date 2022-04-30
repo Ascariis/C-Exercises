3922805db8050bfecf94d28bc16f888ec0bfdaa5
Ascariis <68233308+Ascariis@users.noreply.github.com>
2022-04-30 22:33:52 +0200

    Eight Divisors

---
+++ Varie/Eight-Divisors/eight_divisors.cpp
@@ -0,0 +1,41 @@
+/* The eight divisors of 24 are 1, 2, 3, 4, 6, 8, 12 and 24. The ten numbers not
+exceeding 100 having exactly eight divisors are 24, 30, 40, 42, 54, 56, 66, 70,
+78 and 88. Let f(n) be the count of numbers not exceeding n with exactly eight
+divisors. You are given f(100) = 10, f(1000) = 180 and f(106) = 224427. Find
+f(1012). */
+
+#include <iostream>
+using namespace std;
+
+long valore = 1000;
+
+int qtDivisori(int num) { // Trova la quantità di numeri divisibili per "num"
+
+  int divisori = 2;
+
+  for (int i = 2; i <= num - 1; i++) {
+    if (num % i == 0)
+      divisori++;
+  }
+  return divisori;
+}
+
+int solve() {
+  int ncount = 0;
+  int divisors = 0;
+
+  for (int i = 1; i <= valore; ++i) { // Val da 1 a valore (100)
+    cout << i << endl;
+    divisors = qtDivisori(i); // Qt di divisori per il valore i
+
+    if (divisors == 8)
+      ncount++;
+  }
+
+  return ncount;
+}
+
+int main() {
+  int ris = solve();
+  cout << ris;
+}
